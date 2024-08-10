8bab1a945a2c8e344f8b0f38136905025695d9f5
rbollar <rick@bollar.com>
2024-08-10 10:26:30 +1000

    Add IP screen

--- project/display.c
+++ project/display.c
@@ -10,23 +10,22 @@ Demo for ssd1306 i2c driver for  Raspberry Pi
 
 int main(void) 
 {
-	uint8_t symbol = 0;
-	
-	if(lcd_begin())      //LCD Screen initialization
-	{
-		return 0;
-	}
-	sleep(1);
-	while(1)
-	{
-		lcd_display(symbol);
-		sleep(1);
-        sleep(1);
-		symbol++;
-		if(symbol==4)
+    uint8_t symbol = 0;
+    
+    if(lcd_begin())      //LCD Screen initialization
+    {
+        return 0;
+    }
+    sleep(1);
+    while(1)
+    {
+        lcd_display(symbol);
+        sleep(2); // Keep the sleep time as you prefer, increased to 2 seconds for better readability
+        symbol++;
+        if(symbol==5)  // We now have 5 screens (CPU, RAM, Temp, Disk, IP)
         {
           symbol=0;
         }
-	}
-	return 0;
+    }
+    return 0;
 }
